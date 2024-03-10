df001314f461e474c66c642e56da19011110394d
JaeDoo <fosong98@gmail.com>
2024-03-10 20:01:32 +0900

    feat: Add project initial files
    * 이 프로젝트의 목표는 지도에 사용자가 저장하고 싶은 위치와 정보를
    저장, 공유하는 것이다.

---
+++ src/main/java/site/jaedoo/mygeorecord/MyGeoRecordApplication.java
@@ -0,0 +1,13 @@
+package site.jaedoo.mygeorecord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGeoRecordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyGeoRecordApplication.class, args);
+    }
+
+}
---
+++ src/test/java/site/jaedoo/mygeorecord/MyGeoRecordApplicationTests.java
@@ -0,0 +1,13 @@
+package site.jaedoo.mygeorecord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyGeoRecordApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
